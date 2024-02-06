a06ee598bc056a79d389745ebae3744272518316
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-05 20:12:23 -0700

    Add test to return one merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -21,4 +21,37 @@ describe "Merchants API" do
       expect(merchant[:attributes][:name]).to be_an(String)
     end
   end
+
+    it "sends a merchant based on ID" do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/#{id}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant[:data].count).to eq(3)
+
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data]).to be_a(Hash)
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_an(String)
+
+  end
+
+  describe 'sad paths' do
+    it "will gracefully handle if a book id doesn't exist" do
+      get "/api/v1/merchants/1"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:title]).to eq("Couldn't find Merchant with 'id'=1")
+    end
+  end
 end
