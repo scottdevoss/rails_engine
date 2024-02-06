1309238ac1bc68f6bd42c4df2154c082ae065e01
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-05 20:10:05 -0700

    Add show action and not_found response

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::MerchantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def index
     render json: MerchantSerializer.new(Merchant.all)
+  end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  private
+
+  def not_found_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+    .serialize_json, status: :not_found
   end
 end
