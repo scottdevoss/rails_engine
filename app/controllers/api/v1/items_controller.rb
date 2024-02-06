e63e108410b272f5d8d62cd9da79b408154c9294
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-05 20:09:38 -0700

    Add show action and not found response

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def index
     render json: ItemSerializer.new(Item.all)
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+
+  private
+
+  def not_found_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+    .serialize_json, status: :not_found
   end
 end
