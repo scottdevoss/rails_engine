a3774b88ff851dff3dc38e4090b5c5159a48cbbb
Scott DeVoss <scottdevoss@yahoo.com>
2024-02-07 14:43:35 -0700

    added test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -130,6 +130,24 @@ describe "Items API" do
     merchant_items = JSON.parse(response.body, symbolize_names: true)
     
     expect(response).to be_successful
+    expect(merchant_items[:data].count).to eq(10)
+
+    merchant_items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
   end
 
   describe 'sad paths' do
